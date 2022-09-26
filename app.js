d3dcefc5422d8a9c293bf0d667458ab6e57b15ab
fufako <patryko.mikiciuk@gmail.com>
2022-09-26 12:06:51 +0200

    Setup css

--- app.js
+++ app.js
@@ -16,28 +16,11 @@ app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.use("/", indexRouter)
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get("env") === "development" ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render("error")
-})
-
-module.exports = app
 
 app.listen(3000, () => {
   console.log("Server running on port 3000")
 })
+module.exports = app
