4c8ec2b673b8b3d1976c6f9245b7bb2804a5eb5c
fufako <patryko.mikiciuk@gmail.com>
2022-09-27 13:58:16 +0200

    Change PORT form static to dynamic

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render("error")
 })
-
-app.listen(3000, () => {
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
   console.log("Server running on port 3000")
 })
 module.exports = app
